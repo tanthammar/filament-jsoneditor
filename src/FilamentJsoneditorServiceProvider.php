8adbafef96a430abc359043531650afacd05704a
Tina Hammar <tinahammar@gmail.com>
2023-04-12 16:05:40 +0200

    filament v3

--- src/FilamentJsoneditorServiceProvider.php
+++ src/FilamentJsoneditorServiceProvider.php
@@ -2,54 +2,41 @@
 
 namespace InvadersXX\FilamentJsoneditor;
 
-use Filament\PluginServiceProvider;
+use Filament\Support\Assets\AssetManager;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
 use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentJsoneditorServiceProvider extends PluginServiceProvider
+class FilamentJsoneditorServiceProvider extends PackageServiceProvider
 {
-    public static string $name = 'filament-jsoneditor';
-
-    /**
-     * @var string[]
-     */
-    protected array $scripts = [
-        'invaders-filament-jsoneditor' => __DIR__.'/../dist/jsoneditor/jsoneditor.min.js',
-    ];
-
-    /**
-     * @var string[]
-     */
-    protected array $styles = [
-        'invaders-filament-jsoneditor' => __DIR__.'/../dist/jsoneditor/jsoneditor.min.css',
-    ];
-
-    public function boot()
-    {
-        $this->bootLoaders();
-        $this->bootPublishing();
-    }
-
-    protected function bootPublishing()
-    {
-        if (! $this->app->runningInConsole()) {
-            return;
-        }
-
-        $this->publishes([
-            __DIR__.'/../dist/jsoneditor/img/jsoneditor-icons.svg' => public_path('filament/assets/img/jsoneditor-icons.svg'),
-        ], 'filament-jsoneditor-img');
-    }
-
-    protected function bootLoaders()
-    {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'filament-jsoneditor');
-    }
 
     public function configurePackage(Package $package): void
     {
         $package
-            ->name(self::$name)
+            ->name('filament-jsoneditor')
             ->hasConfigFile()
             ->hasViews();
     }
+
+    public function packageRegistered(): void
+    {
+        $this->app->resolving(AssetManager::class, function () {
+            \Filament\Support\Facades\FilamentAsset::register([
+                Css::make('invaders-filament-jsoneditor-css', __DIR__.'/../dist/jsoneditor/jsoneditor.min.css'),
+                Js::make('invaders-filament-jsoneditor-js', __DIR__.'/../dist/jsoneditor/jsoneditor.min.js')
+            ], 'InvadersXX/filament-jsoneditor');
+
+        });
+    }
+
+    public function packageBooted(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../dist/jsoneditor/img/jsoneditor-icons.svg' => public_path('filament/assets/img/jsoneditor-icons.svg'),
+            ], 'invaders-filament-jsoneditor-img');
+        }
+    }
+
 }
